eea4da020a104554fa06f196c92902556f6f6836
yashpaz123 <46235351+yashpaz123@users.noreply.github.com>
2021-11-28 22:22:31 -0500

    Update mm-implicit.c

--- mm-implicit.c
+++ mm-implicit.c
@@ -32,7 +32,13 @@ init_chunk(header_t *p, size_t csz, bool allocated)
 header_t *
 next_chunk(header_t *h)
 {
-	//Your code here
+    if(h == NULL) {
+        if(mem_heapsize() == 0) return NULL;
+        return mem_heap_lo();
+    }
+    if((size_t)h + h->size >= (size_t)mem_heap_hi()) return NULL;
+    
+    return (header_t*)((void*)h + h->size);
 }
 
 
@@ -48,7 +54,6 @@ int mm_init(void)
 	return 0;
 }
 
-
 // helper function first_fit traverses the entire heap chunk by chunk from the begining. 
 // It returns the first free chunk encountered whose size is bigger or equal to "csz".  
 // It returns NULL if no large enough free chunk is found.
@@ -56,7 +61,14 @@ int mm_init(void)
 header_t *
 first_fit(size_t csz)
 {
-	//Your code here
+    header_t* curr = mem_heap_lo();
+    while(true) {
+        if(!curr->allocated) {
+            if(curr->size >= csz) return curr;
+        }
+        curr = next_chunk(curr);
+        if(curr == NULL) return NULL;
+    }
 }
 
 // helper function split cuts the chunk into two chunks. The first chunk is of size "csz", 
@@ -65,7 +77,11 @@ first_fit(size_t csz)
 void
 split(header_t *original, size_t csz)
 {
-	//Your code here
+	size_t chunk_size = original->size;
+    if(chunk_size <= csz + hdr_size) return;
+    original->size = csz;
+    header_t* hdr2 = (header_t*)((void*)original + csz);
+    hdr2->size = chunk_size - csz;
 }
 
 // helper function ask_os_for_chunk invokes the mem_sbrk function to ask for a chunk of 
@@ -74,7 +90,9 @@ split(header_t *original, size_t csz)
 header_t *
 ask_os_for_chunk(size_t csz)
 {
-	//Your code here
+    header_t* hdr = mem_sbrk((int)csz);
+    init_chunk(hdr, csz, false);
+    return hdr;
 }
 
 /* 
@@ -99,14 +117,22 @@ mm_malloc(size_t size)
 	//    If found, split the chunk (using helper function split).
 	//    If not found, ask OS for new memory using helper ask_os_for_chunk
 	//Set the chunk's status to be allocated
-
+    
+    p = first_fit(csz);
+    if(p == NULL) {
+        p = ask_os_for_chunk(csz);
+    }
+    else {
+        split(p, csz);
+    }
+    p->allocated = true;
 
 	//After finishing obtaining free chunk p, 
 	//check heap correctness to catch bugs
 	if (debug) {
 		mm_checkheap(true);
 	}
-	return (void *)p;
+	return (void*)p + hdr_size;
 }
 
 // Helper function payload_to_header returns a pointer to the 
@@ -114,8 +140,7 @@ mm_malloc(size_t size)
 header_t *
 payload2header(void *p)
 {
-	//Your code here
-
+    return (header_t*)(p - hdr_size);
 }
 
 // Helper function coalesce merges free chunk h with subsequent 
@@ -124,7 +149,11 @@ payload2header(void *p)
 void
 coalesce(header_t *h)
 {
-	//Your code here
+    while(true) {
+        header_t* next = next_chunk(h);
+        if(next == NULL || next->allocated) return;
+        h->size += next->size;
+    }
 }
 
 /*
@@ -139,8 +168,11 @@ mm_free(void *p)
 	// Obtain pointer to current chunk using helper payload_to_header 
 	// Set current chunk status to "free"
 	// Call coalesce() to merge current chunk with subsequent free chunks
-	  
-	  
+    
+    header_t* hdr = payload2header(p);
+    hdr->allocated = false;
+    coalesce(hdr);
+    
 	// After freeing the chunk, check heap correctness to catch bugs
 	if (debug) {
 		mm_checkheap(true);
@@ -157,13 +189,35 @@ mm_free(void *p)
 void *
 mm_realloc(void *ptr, size_t size)
 {
-	// Your code here
-	  
+    void* new_ptr = ptr;
+    header_t* hdr = payload2header(ptr);
+    size_t csz = hdr_size + align(size);
+    if(csz <= hdr->size) {
+        size_t amount_decr = hdr->size - csz;
+        hdr->size = csz;
+        if(amount_decr >= hdr_size) {
+            // blah
+        }
+    }
+    else {
+        size_t amount_incr = csz - hdr->size;
+        header_t* next_hdr = next_chunk(hdr);
+        if(!next_hdr->allocated && next_hdr->size >= amount_incr) {
+            split(next_hdr, amount_incr);
+            hdr->size = csz;
+        }
+        else {
+            new_ptr = mm_malloc(size);
+            memcpy(new_ptr, ptr, hdr->size - hdr_size);
+            mm_free(ptr);
+        }
+    }
+    
 	// Check heap correctness after realloc to catch bugs
 	if (debug) {
 		mm_checkheap(true);
 	}
-	return NULL;
+	return new_ptr;
 }
 
 
@@ -175,12 +229,23 @@ mm_realloc(void *ptr, size_t size)
 heap_info_t 
 mm_checkheap(bool verbose) 
 {
-	heap_info_t info;
-	// Your code here
-	// 
-	// traverse the heap to fill in the fields of info
-
+    heap_info_t info = {0};
+    
+    header_t* hdr = next_chunk(NULL);
+    while(hdr != NULL) {
+        if(hdr->allocated) {
+            info.num_allocated_chunks++;
+            info.allocated_size += hdr->size;
+        }
+        else {
+            info.num_free_chunks++;
+            info.free_size += hdr->size;
+        }
+        hdr = next_chunk(hdr);
+    }
+    
 	// correctness of implicit heap amounts to the following assertion.
 	assert(mem_heapsize() == (info.allocated_size + info.free_size));
 	return info;
 }
+
